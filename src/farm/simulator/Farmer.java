8b6257cc04d29acf57d71c7acb689d664d697b03
LewisMarshall465 <41558108+LewisMarshall465@users.noreply.github.com>
2020-05-05 14:39:40 +1200

    Construction of various base classes
    class Item, class Farmer, class Crop

--- src/farm/simulator/Farmer.java
+++ src/farm/simulator/Farmer.java
@@ -1,5 +1,83 @@
 package farm.simulator;
 
-public class Farmer {
+import java.util.ArrayList;
 
+/**
+ *
+ * This abstract class implements a farmer, the user's character.
+ *
+ * @author Lewis Marshall
+ * @author Megan Steenkamp
+ */
+
+public abstract class Farmer {
+    private String name;
+    private int age;
+    private ArrayList<item> items = new ArrayList<~>();
+
+    /**
+     * Class constructor for the Farmer class
+     *
+     * @param name                  The name of the farmer
+     * @param age                   The age of the farmer
+     * @param items                 The items the farmer is in possession of
+     */
+
+    /**
+     * Sets the name of the farmer
+     *
+     * @param name of the farmer
+     */
+    public void setName(String name) {this.name = name;    }
+
+    /**
+     * Returns the name of a farmer.
+     *
+     * @return name of the farmer
+     */
+    public String getName() {return name;}
+
+    /**
+     * Sets the name of the farmer
+     *
+     * @param name of the farmer
+     */
+    public void setAge(int age) {this.age = age;}
+
+    /**
+     * Returns the age of a farmer.
+     *
+     * @return age of the farmer
+     */
+    public int getAge() {return age;}
+
+    public void setItems(ArrayList<item> items) {this.items = items;}
+
+    /**
+     * Returns the inventory of the farmer's items.
+     *
+     * @return age of the farmer
+     */
+
+    public ArrayList<item> getItems() {return items;}
+
+    public void tendToCrop(item) {
+
+    }
+
+    public void feedAnimals(item) {
+
+    }
+
+    public void playWithAnimals() {
+
+    }
+
+    public void harvestCrops() {
+
+    }
+
+    public void tendToCrops() {
+
+    }
 }
--- src/farm/simulator/Item.java
+++ src/farm/simulator/Item.java
@@ -1,5 +1,99 @@
 package farm.simulator;
 
-public class Item {
+/**
+ * The abstract class implementing the base class for all items.
+ *
+ * @author Lewis Marshall
+ * @author Megan Steenkamp
+ */
 
+public abstract class Item {
+
+    private float price;
+    private String name;
+    private float cropGrowthFactor;
+    private float animalHealthFactor;
+    private String description;
+
+    /**
+     * Class constructor for the Item class
+     *
+     * @param price                 price of the item
+     * @param name                  name of the item
+     * @param cropGrowthFactor      scaling factor for crop growth
+     * @param animalHealthFactor    scaling factor for animal health
+     * @param description           description of the item
+     */
+
+    // TODO: tostring() method
+
+    /**
+     * Sets the price of an item.
+     *
+     * @param price of an item.
+     */
+    public void setPrice(float price) {this.price = price;}
+
+    /**
+     * Returns the price of an item.
+     *
+     * @return price of an item.
+     */
+    public float getPrice() {return price;}
+
+    /**
+     * Sets the name of an item.
+     *
+     * @param name of an item.
+     */
+    public void setName(String name) {this.name = name;}
+
+    /**
+     * Returns the name of an item.
+     *
+     * @return name of an item.
+     */
+    public String getName() {return name;}
+
+    /**
+     * Sets the factor for scaling crop growth for an item.
+     *
+     * @param cropGrowthFactor of an item.
+     */
+    public void setCropGrowthFactor(float cropGrowthFactor) {this.cropGrowthFactor = cropGrowthFactor;}
+
+    /**
+     * Returns the factor to scale crop growth time by when using an item.
+     *
+     * @return cropGrowthFactor of an item.
+     */
+    public float getCropGrowthFactor() {return cropGrowthFactor;}
+
+    /**
+     * Sets the factor for scaling animal health for an item.
+     *
+     * @param animalHealthFactor of an item.
+     */
+    public void setAnimalHealthFactor(float animalHealthFactor) {this.animalHealthFactor = animalHealthFactor;}
+
+    /**
+     * Returns the factor to scale animal health by when using an item.
+     *
+     * @return animalHealthFactor of an item.
+     */
+    public float getAnimalHealthFactor() {return animalHealthFactor;}
+
+    /**
+     * Sets the description for an item.
+     *
+     * @param description for an item.
+     */
+    public void setDescription(String description) {this.description = description;}
+
+    /**
+     * Returns the description for an item.
+     *
+     * @return description for an item.
+     */
+    public String getDescription() {return description;}
 }
